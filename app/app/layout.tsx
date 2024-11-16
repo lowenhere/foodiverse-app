39ceeab9dda3b9816808bd023e24d7ede2f7ed2d
lowen <lowenhere@gmail.com>
2024-11-16 15:55:35 +0700

    build fixes

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,11 +1,12 @@
 "use client";
+import { ReactNode } from "react";
 
 import { Web3Providers } from "@/components/providers/Web3Providers";
 
 export default function AppLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return <Web3Providers>{children}</Web3Providers>;
 }
--- app/app/login/layout.tsx
+++ app/app/login/layout.tsx
@@ -1,9 +1,10 @@
 "use client";
+import { ReactNode } from "react";
 
 export default function LoginLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <main className="container-nd flex flex-col items-center justify-center text-center h-screen p-4">
