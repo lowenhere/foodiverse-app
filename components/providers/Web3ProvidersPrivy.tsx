2f25f75ebaa1930deabb2e306a8c3fd68cf5da9d
lowen <lowenhere@gmail.com>
2024-11-16 11:29:50 +0700

    compile fixes

--- components/providers/Web3ProvidersPrivy.tsx
+++ components/providers/Web3ProvidersPrivy.tsx
@@ -6,21 +6,17 @@ import { http, createConfig } from "wagmi";
 import { WagmiProvider } from "@privy-io/wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { mantle } from "viem/chains";
-import { StorySmartWalletProvider } from "@/contexts/StorySmartWalletContext";
-import { odyssey } from "@story-protocol/core-sdk";
 
-export default function Web3ProvidersPrivy({ children }: PropsWithChildren) {
+export function Web3ProvidersPrivy({ children }: PropsWithChildren) {
   const queryClient = new QueryClient();
 
   const wagmiConfig = createConfig({
-    chains: [odyssey, mantle],
+    chains: [mantle],
     transports: {
-      [odyssey.id]: http(),
       [mantle.id]: http(),
     },
   });
 
-export function Web3ProvidersPrivy({ children }: PropsWithChildren) {
   const privyAppId = process.env.NEXT_PUBLIC_PRIVY_APP_ID;
 
   if (!privyAppId) {
